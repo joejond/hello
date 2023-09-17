839885ee7c096f014d3ac6b533e078928f26fe75
joejond <jono.daunbiru@gmail.com>
2023-09-17 23:12:08 +0700

    halo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome React Web
+          Welcome React Web Halo
         </a>
       </header>
     </div>
